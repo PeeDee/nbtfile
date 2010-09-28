1aed88559af60f242ae139b703a97d01e26978b9
MenTaLguY <mental@rydia.net>
2010-09-28 02:53:05 -0400

    start on specs

--- spec/nbtfile_spec.rb
+++ spec/nbtfile_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'nbtfile'
 
-describe "Nbtfile" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe NBTFile do
+  it "exists" do
   end
 end
